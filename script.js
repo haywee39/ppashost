0486cf63b53a3a2d42fa939c1998d2ccfc7f762d
Your Name <[haywee39@gmail.com]>
2025-01-23 13:44:58 +0100

    new section for price 23jan

--- script.js
+++ script.js
@@ -89,3 +89,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", handleScroll);
 });
+
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// _****************************************************************
+
+// *******************************************************************
+// JavaScript to handle card flip on "See Details" and "Back" button clicks
+function flipCard(event) {
+    const card = event.target.closest('.menu-card');
+    card.classList.add('flip');
+  }
+  
+  function flipBack(event) {
+    const card = event.target.closest('.menu-card');
+    card.classList.remove('flip');
+  }
+ 
